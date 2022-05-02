e86559074025f428596fdcf0de475f76d67e97a3
Damour8333 <98494743+Damour8333@users.noreply.github.com>
2022-05-02 23:55:48 +0200

    ajuster main marge

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var closeBtn = document.getElementById("closeBtn");
 openBtn.onclick = openNav;
 closeBtn.onclick = closeNav;
 
-/* Set the width of the side navigation to 250px */
+
 function openNav() {
   sidenav.classList.add("active");
 }
